306ac843d3e4642629ef9a15a5843e52a0f314d9
Andrew John McAteer <ajmcateer@gmail.com>
2018-04-16 00:55:20 -0400

    Added a ping method and several overloads

--- NetworkTools/Ping.cs
+++ NetworkTools/Ping.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NetworkTools
 {
-    class Ping
+    public static class SendPing
     {
+        public static async Task<long> Ping(string url)
+        {
+            return (await new Ping().SendPingAsync(url)).RoundtripTime;
+        }
+
+        public static async Task<long> Ping(IPAddress ip)
+        {
+            return (await new Ping().SendPingAsync(ip)).RoundtripTime;
+        }
+
+        public static async Task<long> Ping(string url, int timeout)
+        {
+            return (await new Ping().SendPingAsync(url, timeout)).RoundtripTime;
+        }
+
+        public static async Task<long> Ping(IPAddress ip, int timeout)
+        {
+            return (await new Ping().SendPingAsync(ip, timeout)).RoundtripTime;
+        }
     }
 }
